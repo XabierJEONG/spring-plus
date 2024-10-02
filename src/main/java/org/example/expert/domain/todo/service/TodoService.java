912707da2e39075b220207136aebea2491db9e47
Xabier <94465466+XabierJEONG@users.noreply.github.com>
2024-10-02 20:17:46 +0900

    4. 테스트 코드 퀴즈 - 컨트롤러 테스트의 이해 & 1. @Transactional 수정

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -19,7 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-
+@Transactional(readOnly = true)
 public class TodoService {
 
     private final TodoRepository todoRepository;
@@ -48,7 +48,6 @@ public class TodoService {
         );
     }
 
-    @Transactional(readOnly = true)
     public Page<TodoResponse> getTodos(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
@@ -65,7 +64,6 @@ public class TodoService {
         ));
     }
 
-    @Transactional(readOnly = true)
     public TodoResponse getTodo(long todoId) {
         Todo todo = todoRepository.findByIdWithUser(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
